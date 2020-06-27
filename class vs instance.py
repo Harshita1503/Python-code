7fd1b1dc98ba2939b9b895736686e7ed98d5b124
Harshita1503 <55319937+Harshita1503@users.noreply.github.com>
2020-06-27 23:12:46 +0530

    Create class vs instance.py

---
+++ class vs instance.py
@@ -0,0 +1,29 @@
+'''
+In this challenge, we're going to learn about the difference between a class and an instance;
+because this is an Object Oriented concept, it's only enabled in certain languages. 
+Check out the Tutorial tab for learning materials and an instructional video!!'''
+
+
+class Person:
+    def __init__(self, initialAge):
+        # Add some more code to run some checks on initialAge
+        self.age = 0
+        if initialAge < 0:
+            print("Age is not valid, setting age to 0.")
+        else:
+            self.age = initialAge
+
+
+    def amIOld(self):
+        # Do some computations in here and print out the correct statement to the console
+        if age < 13:
+            print("You are young.")
+        elif 13 <= age < 18:
+            print("You are a teenager.")
+        elif age >= 18:
+            print("You are old.")
+
+    def yearPasses(self):    
+        # Increment the age of the person in here
+        global age
+        age += 1
