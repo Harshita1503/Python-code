9627f156788812137d3e12c6712530162feb844b
Harshita1503 <55319937+Harshita1503@users.noreply.github.com>
2020-07-03 20:53:13 +0530

    Create array mathematics.py

---
+++ array mathematics.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+n,m = list(map(int,input().split()))
+a=np.array([input().split() for i in range(n)],int)
+b=np.array([input().split() for i in range(n)],int)
+
+
+print (a + b) 
+print (a - b )
+print (a * b )
+print (a//b )
+print (a % b)
+
+print (a**b  )
